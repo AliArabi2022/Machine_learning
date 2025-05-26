8fa0ef7ee939271f6454319cafd78f27f4bb8381
AliArabi2022 <aliscience2022@gmail.com>
2025-05-26 23:57:58 +0330

    Logestic_regression part1

---
+++ Supervised_learning/Logestic Regression/Logestic_regression.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.datasets import load_breast_cancer
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+
+
+data = load_breast_cancer()
+X = data.data
+y = data.target 
+print (data.feature_name)
