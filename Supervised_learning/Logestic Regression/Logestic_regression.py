27d0cec982ec926dd54479f6cb8d4a6afe603368
AliArabi2022 <aliscience2022@gmail.com>
2025-05-31 03:56:18 +0330

    update-visualazation

--- Supervised_learning/Logestic Regression/Logestic_regression.py
+++ Supervised_learning/Logestic Regression/Logestic_regression.py
@@ -9,4 +9,14 @@ from sklearn.metrics import confusion_matrix
 data = load_breast_cancer()
 X = data.data
 y = data.target 
-print (data.feature_name)
+print (data.feature_names)
+# we will only take two features for visualization purposes
+X= data.data[:,[2,4]]# 'mean perimeter' & 'mean smoothness'
+
+# Standardizing the features
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
+# add bias (x_0 = 1) term to X
+X_bias = np.hstack((X, np.ones((X.shape[0],1))))
+print(X_bias)
